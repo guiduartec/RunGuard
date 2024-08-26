934f4c59943622f940de477b604b058e887c21b5
murilo-martinez <murilo.martinez@sptech.school>
2024-08-26 14:17:44 -0300

    Implementação do script python e implementação da novo script de BD

--- BANCO DE DADOS/BancoDeDados-RunGuard.sql
+++ BANCO DE DADOS/BancoDeDados-RunGuard.sql
@@ -1,9 +1,11 @@
+-- CRIAÇÃO DE DATABASE
 drop database if exists runguard;
-
 create database runguard;
 use runguard;
 
 
+
+-- CRIAÇÃO DE TABELAS
 create table empresa (
 idEmpresa int primary key auto_increment,
 nome_empresa varchar(45)
@@ -12,12 +14,14 @@ nome_empresa varchar(45)
 create table usuario (
 idUsuario int primary key auto_increment,
 nome_usuario varchar(45),
-email_usuario varchar(45),
+email_usuario varchar(45) unique,
 senha_usuario varchar(45),
 cargo varchar(45),
 fkEmpresa_usuario int,
     constraint fkEmpresaUsuario foreign key (fkEmpresa_usuario) references empresa (idEmpresa)
 );
+
+select * from usuario;
 
 create table equipamento (
 idEquipamento int primary key auto_increment,
@@ -31,21 +35,33 @@ idDados int primary key auto_increment,
 cpu_porcent double,
 memoria_porcent double,
 memoria_usada double,
-disco_porcent_usado double,
 dtHora datetime default current_timestamp,
 fkEquipamento int,
     constraint fkEquipamentoDados foreign key (fkEquipamento) references equipamento (idEquipamento)
 );
 
-select * from dados;
-
 insert into empresa values
 (default, 'Uber'),
 (default, '99 Táxi');
-
 
 insert into equipamento values
 (default, 'M1', 1),
 (default, 'M2', 1),
 (default, 'M3', 1),
 (default, 'M4', 1);
+
+CREATE VIEW Monitoramento as
+SELECT 
+    d.idDados AS ID,
+    CONCAT(d.cpu_porcent, "%") AS "Porcentagem CPU",
+    CONCAT(d.memoria_porcent, "%") AS "Porcentagem Memoria",
+    CONCAT(d.memoria_usada, "GB") AS "Memoria usada",
+    CONCAT(d.disco_porcent_usado, "%") AS "Porcentagem de Disco Usada",
+    d.dtHora AS "Data",
+    e.nome_equipamento AS Equipamento
+FROM 
+    dados as d
+JOIN 
+    equipamento as e ON d.fkEquipamento = e.idEquipamento;
+
+SELECT * FROM Monitoramento;
---
+++ SCRIPT PYTHON/conexao.py
@@ -0,0 +1,53 @@
+# API do Captura dados - Sprint 1 - RunGuard
+
+# Importação das bibliotecas
+import psutil
+import time
+import mysql.connector
+
+# Criação da conexão do Banco de Dados
+mydb = mysql.connector.connect(
+  host="10.18.32.37",
+  user="martinez",
+  password="SPTech#2024",
+  database="runguard",
+  port=3306 
+)
+
+mycursor = mydb.cursor()
+
+run = True
+
+# Função para converter os valores de Bytes para Gigabytes
+def byte_para_gb(byte):
+    return byte / (1024 ** 3)
+
+# Loop para capturar os dados e cadastrar no Banco de Dados
+while run:
+    print('=======================')
+    
+    # 
+    cpu = psutil.cpu_percent() # Obtém quanto a CPU está em porcentagem
+    memoria = psutil.virtual_memory() # Obtém quanto a Memória está em porcentagem
+    
+    memoria_usada = byte_para_gb(memoria.used) # Converte os bytes para Gigabyte do memória usada
+
+    memoria_usada_formatada = f'{memoria_usada:.1f}' # Formata o número para melhor a gravação no banco
+
+    # Imprime as informações no terminal para uma visualização
+    print(f'A CPU está em {cpu} %')
+    print(f'A memória está em {memoria.percent} %')
+    print(f'Total de memória usada: {memoria.used} GB')
+
+    # Faz as inserções no banco de dados passando os componentes
+    sql = "INSERT INTO dados (cpu_porcent, memoria_porcent,memoria_usada,) VALUES (%s, %s, %s, %s, 'M1')"
+
+    # Captura os valores
+    val = (cpu,memoria.percent,memoria_usada_formatada)
+
+    # Executa a query e os valores
+    mycursor.execute(sql,val)
+
+    mydb.commit()
+
+    time.sleep(3)
